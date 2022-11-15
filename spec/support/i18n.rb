6a85ed5101f9df13ed90281bf5f7a4cdd269d519
Christophe Bliard <c.bliard@openproject.com>
2022-11-15 08:39:09 +0100

    Restrict locales to :en to avoid a 2 secs penalty
    Loading all translation files in all locales can take 2 to 3 seconds.
    This is most noticeable when running a small set of tests where the
    loading lag makes the output stop during 2 seconds. Reducing the number
    of loaded translation files reduces this lag.
    
    To make tests relying on other locales pass, additional locales are
    lazily loaded when `Redmine::I18n#set_language_if_valid` is called.

---
+++ spec/support/i18n.rb
@@ -0,0 +1,67 @@
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2022 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+# Restricts loaded locales to :en to avoid a 2-seconds penalty when locales are
+# loaded.
+#
+# Additional locales are lazily loaded when
+# +Redmine::I18n#set_language_if_valid+ is called
+module I18nLazyLoadingPatch
+  # overrides Redmine::I18n#set_language_if_valid
+  def set_language_if_valid(lang)
+    if locale = find_language(lang)
+      I18nLazyLoadingPatch.load_locale(locale)
+      super
+    end
+  end
+
+  def self.install
+    # copy original I18n load path
+    @@original_load_path = I18n.config.load_path.dup
+    # restrict available locales to :en
+    I18n.config.load_path = load_path(:en)
+    # Hook into Redmine::I18n to load new locales when needed
+    Redmine::I18n.prepend(self)
+  end
+
+  def self.load_locale(locale)
+    return if ::I18n.config.available_locales_set.include?(locale)
+
+    I18n.backend.load_translations(load_path(locale))
+    I18n.config.clear_available_locales_set
+  end
+
+  def self.load_path(locale)
+    file_regex = /#{locale}\.(rb|yml)$/
+    @@original_load_path.grep(file_regex)
+  end
+end
+
+RSpec.configure do
+  I18nLazyLoadingPatch.install
+end
